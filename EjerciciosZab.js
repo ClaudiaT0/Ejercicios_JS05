333beb38c81f606b88c722db2885a81e95025979
ClaudiaT0 <claudiatalaveraor@gmail.com>
2023-09-22 00:09:20 -0600

    se agrega ejercicio

--- EjerciciosZab.js
+++ EjerciciosZab.js
@@ -10,3 +10,49 @@ function datosPersona(username, age, movie){
     //console.log(`Esribe tu movie ${movie}`)
     console.log(`The film: ${movie} is one of my favorite`)
 }
+
+/**
+ * Highest number
+Write a program that asks for 10 numbers. 
+Using logical operators and any other javascript
+functions/structures you've seen before, 
+determine and output the highest of those numbers.
+ * 
+ */
+
+function numeroMayor(){
+}
+
+/**
+ * Alarm
+Write a program that asks a user for the amount of seconds 
+needed until an alarm (message) is executed alongside a text 
+to show once those seconds have passed in real time.
+Result example: "Time for bed after 10 seconds".
+ */
+function programAlarm(time) {
+    
+    
+}
+
+/**
+ * Palindrome
+ Write a program that prompts for a word or sentence
+ (it can be capitalized, have spaces and punctuation). 
+ Figure out if the sentence/word is a palindrome or not.
+ Ignoring punctuation, spaces and capitalized letters.
+ */
+
+
+ /**
+  * Factorial
+  Write a program that prompts for an intenger number n.
+  Where  1 <= n. Solve this using recursion.
+  */
+
+/**
+ * Flat array
+Write a program that takes the following nested matrix and flattens it
+(makes it a 1D array). Use any type of algorithm you want like callbacks, 
+recursion, etc...
+ */
